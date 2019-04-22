c3641370a3438caea482d53e7b6f46b66a9c98c8
diper1998 <perov.dima.dima@gmail.com>
2019-04-22 12:42:14 +0300

    Update ItrSearch

--- modules/interpolation-search/include/ItrSearch.h
+++ modules/interpolation-search/include/ItrSearch.h
@@ -1,27 +1,26 @@
 // Copyright 2019 Perov Dima
 
-#ifndef MODULES_INTERPOLATION_SEARCH_INCLUDE_ITRSEARCH_H_ 
+#ifndef MODULES_INTERPOLATION_SEARCH_INCLUDE_ITRSEARCH_H_
 #define MODULES_INTERPOLATION_SEARCH_INCLUDE_ITRSEARCH_H_
 
 #include<string>
 
 class ItrSearch {
-
     protected:
-     unsigned* ptrData;
-     unsigned sizeData;
-     int findIndex;
+       unsigned* ptrData;
+       unsigned sizeData;
+       int findIndex;
 
     public:
-     ItrSearch(unsigned* myPtrSortedData, unsigned  mySizeData);
-     ItrSearch();
-     ~ItrSearch();
-     int GetFindIndex();
-     unsigned GetSizeData();
-     unsigned* GetPtrData(); 
-     void SetSortedData(unsigned* myPtrData, unsigned mySizeData);
-     int Search(unsigned toFind);
-     int Search(unsigned toFind, unsigned myLeft, unsigned myRight);
+       ItrSearch(unsigned* myPtrSortedData, unsigned  mySizeData);
+       ItrSearch();
+       ~ItrSearch();
+       int GetFindIndex();
+       unsigned GetSizeData();
+       unsigned* GetPtrData();
+       void SetSortedData(unsigned* myPtrData, unsigned mySizeData);
+       int Search(unsigned toFind);
+       int Search(unsigned toFind, unsigned myLeft, unsigned myRight);
 };
 
-#endif // MODULES_INTERPOLATION_SEARCH_INCLUDE_ITRSEARCH_H_"
+#endif  // MODULES_INTERPOLATION_SEARCH_INCLUDE_ITRSEARCH_H_"
